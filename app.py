5e9729d503f85ac203bdae655ad554a33a4cb8fd
Ankit mishra <143910171+theankitghotala@users.noreply.github.com>
2026-04-11 05:14:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ if show_metrics:
     # Technical Summary
     st.subheader("Objectives & Dataset Summary")
     col_a, col_b, col_c = st.columns(3)
-    col_a.metric("Data Split", "88% Train / 8% Test / 4% Validation")
+    col_a.metric("Data Split", "77% Train / 11% Test / 12% Validation")
     col_b.metric("Training Epochs", "100")
     col_c.metric("Optimizer", "MuSGD")
     
